de42c41988be3d57d58695f237f97503c5867cec
Arijit Chowdhury <coolari7@gmail.com>
2021-11-02 11:39:37 +0530

    UI Change

--- src/App.js
+++ src/App.js
@@ -18,19 +18,19 @@ export default function App() {
 
   return (
     <div className="m-2 text-sm text-gray-600">
-      <form className="grid gap-2" onSubmit={onFormSubmit}>
+      <form className="grid sm:flex gap-2" onSubmit={onFormSubmit}>
         <input
           type="text"
           value={title}
           onChange={onTitleChange}
           placeholder="Buy eggs..."
-          className="border-2 border-gray-300 p-2 outline-none focus:border-indigo-600"
+          className="border-2 sm:flex-grow border-gray-300 p-2 outline-none focus:border-indigo-600"
         />
         <button
           type="submit"
-          className="outline-none text-white bg-indigo-600 font-bold rounded-sm p-2"
+          className="sm:min-w outline-none text-white bg-indigo-600 font-bold rounded-sm p-2"
         >
-          Add
+          Add Todo
         </button>
       </form>
       <TodoList />
